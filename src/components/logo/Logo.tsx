c3a18e1fad3da46e27cb3e052c1e7edac48c5504
Ali Athary <ali.athary.ce@gmail.com>
2024-11-10 20:41:29 +0330

    logo

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,29 @@
+import { useTheme } from "@mui/material";
+import LogoSvg from "../../assets/typography_logo.svg?react";
+import { brand, white, grayLightMode } from "../../theme/colors";
+import { useNavigate } from "react-router-dom";
+
+const Logo = () => {
+  const navigate = useNavigate();
+  const theme = useTheme();
+  const isDarkMode = theme.palette.mode === "dark";
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <LogoSvg
+      height="40px"
+      onClick={handleClick}
+      style={
+        {
+          "--text-color": isDarkMode ? white : grayLightMode[800],
+          "--logo-color": isDarkMode ? brand[400] : brand[600],
+        } as React.CSSProperties
+      }
+    />
+  );
+};
+
+export default Logo;
