bf821739dc3bf11336c040127f024ad10fd91de4
Ali Athary <ali.athary.ce@gmail.com>
2024-11-15 22:13:41 +0330

    my

--- src/components/RegisterPage/RegisterPage.tsx
+++ src/components/RegisterPage/RegisterPage.tsx
@@ -85,7 +85,7 @@ const RegisterPage = () => {
     } else setPasswordConfirmErr(false);
   };
   return (
-    <Box sx={{ mt: 10 }}>
+    <Box sx={{ my: 10 }}>
       <Stack
         width="100%"
         direction="row"
