53d9da48688bbc5ce0c128a2c8e7cc25a25a8916
Ali Athary <ali.athary.ce@gmail.com>
2025-01-23 18:49:36 +0330

    fix post

--- src/apis/searchPostsApi.ts
+++ src/apis/searchPostsApi.ts
@@ -14,10 +14,22 @@ export interface SearchPostsResponse {
   postOverview: postOverview[];
 }
 
+export interface SearchFilter {
+  categories: string[];
+  ascending: boolean;
+  sort_by: "SORT_BY_UNSPECIFIED" | "CREATED_TIME" | "DEADLINE" | null;
+}
+
 export const searchPostsApi = async (
-  title: string,
-  page: number
+  title: string | null,
+  page: number,
+  filter: SearchFilter | null
 ): Promise<SearchPostsResponse> => {
-  const response = await apiClient.post("/api/search-post", { title, page });
+  console.log("title: ", title, ", filter: ", filter);
+  const response = await apiClient.post("/api/search-post", {
+    title,
+    page,
+    filter,
+  });
   return response.data;
 };
--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -3,42 +3,50 @@ import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import PostPreview from "../../pages/PreviewPage/PostPreview";
 import PrimaryButton from "../buttons/PrimaryButton";
-import { postOverview, searchPostsApi } from "../../apis/searchPostsApi";
+import {
+  postOverview,
+  SearchFilter,
+  searchPostsApi,
+} from "../../apis/searchPostsApi";
 import SingleTextField from "../textField/singleTextField";
 import MultipleSelectChip from "../textField/multipleTextField";
 
 const SearchPage = () => {
   const location = useLocation();
   const selectedCategoryFromNav = location.state?.selectedCategory || null;
-
-  const [selectedCategories, setSelectedCategories] = useState<string[]>(
-    selectedCategoryFromNav ? [selectedCategoryFromNav] : []
-  );
+  const [filter, setFilter] = useState<SearchFilter>({
+    categories: selectedCategoryFromNav ? [selectedCategoryFromNav] : [],
+    ascending: false,
+    sort_by: null,
+  });
 
   const handleCategoriesChange = (categories: string[]) => {
     console.log("Selected Categories:", categories);
-    setSelectedCategories(categories);
+    setFilter((prevFilter) => {
+      return { ...prevFilter, categories: categories };
+    });
   };
 
   const [searchedPosts, setSearchedPosts] = useState<postOverview[]>([]);
   const searchObject = useParams().object;
 
   useEffect(() => {
-    if (searchObject) {
-      console.log("Triggering API call for:", searchObject);
-
-      searchPostsApi(searchObject, 0)
-        .then((response) => {
-          if (response) {
-            console.log("Posts:", response);
-            setSearchedPosts(response.postOverview);
-          }
-        })
-        .catch((err) => {
-          console.log("API Error:", err);
-        });
-    }
+    handleSearch();
   }, [searchObject]);
+
+  const handleSearch = () => {
+    searchPostsApi(searchObject || null, 0, filter)
+      .then((response) => {
+        if (response) {
+          console.log("Posts:", response);
+          setSearchedPosts(response.postOverview);
+        }
+      })
+      .catch((err) => {
+        console.log("API Error:", err);
+      });
+  };
+
   return (
     <Box
       display="flex"
@@ -67,12 +75,17 @@ const SearchPage = () => {
             selects={["محبوب ترین", "جدیدترین", "قدیمی ترین", "پر بازدیدترین"]}
           />
           <MultipleSelectChip
-            categories={selectedCategories}
+            categories={filter.categories}
             onCategoriesChange={handleCategoriesChange}
           />
         </Box>
 
-        <PrimaryButton height="40px" width="260px" text="اعمال فیلترها" />
+        <PrimaryButton
+          height="40px"
+          width="260px"
+          text="اعمال فیلترها"
+          onClick={handleSearch}
+        />
       </Box>
       {searchedPosts ? (
         <Box display="flex" flexDirection="column" gap={2}>
